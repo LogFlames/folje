1dbfb6dc5e1c989b4a8a4eea59ad3c5fcb4992fd
Elias Lundell <elilun03@gmail.com>
2025-01-06 01:49:53 +0100

    hydrapp init

---
+++ go.mod
@@ -0,0 +1,29 @@
+module github.com/LogFlames/folje
+
+go 1.23.1
+
+toolchain go1.23.3
+
+require github.com/pojntfx/hydrapp/hydrapp v0.0.0-20241205024353-0f0a33a6ef35
+
+require (
+	github.com/Kodeworks/golang-image-ico v0.0.0-20141118225523-73f0f4cfade9 // indirect
+	github.com/ProtonMail/go-crypto v1.1.3 // indirect
+	github.com/ProtonMail/go-mime v0.0.0-20230322103455-7d82a3887f2f // indirect
+	github.com/ProtonMail/gopenpgp/v2 v2.8.1 // indirect
+	github.com/akavel/rsrc v0.10.2 // indirect
+	github.com/breml/rootcerts v0.2.19 // indirect
+	github.com/cloudflare/circl v1.5.0 // indirect
+	github.com/dchest/jsmin v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/josephspurrier/goversioninfo v1.4.1 // indirect
+	github.com/ncruces/zenity v0.10.14 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/randall77/makefat v0.0.0-20210315173500-7ddd0e42c844 // indirect
+	golang.org/x/crypto v0.30.0 // indirect
+	golang.org/x/image v0.23.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	yrh.dev/icns v0.0.0-20210608051050-de3ea6a57b33 // indirect
+)
---
+++ main.go
@@ -0,0 +1,93 @@
+//go:build !android
+// +build !android
+
+package main
+
+import (
+	"bytes"
+	"context"
+	_ "embed"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/LogFlames/folje/pkg/backend"
+	"github.com/LogFlames/folje/pkg/frontend"
+	"github.com/pojntfx/hydrapp/hydrapp/pkg/config"
+	_ "github.com/pojntfx/hydrapp/hydrapp/pkg/fixes"
+	"github.com/pojntfx/hydrapp/hydrapp/pkg/ui"
+)
+
+//go:embed hydrapp.yaml
+var configFile []byte
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cfg, err := config.Parse(bytes.NewBuffer(configFile))
+	if err != nil {
+		ui.HandleFatalPanic("App", errors.Join(fmt.Errorf("could not parse config file"), err))
+		return
+	}
+
+	// Apply the self-update
+	browserState := &ui.BrowserState{}
+	go func() {
+		if err := ui.SelfUpdate(
+			ctx,
+			cfg,
+			browserState,
+		); err != nil {
+			ui.HandleFatalPanic(cfg.App.Name, err)
+		}
+	}()
+
+	// Start the backend
+	backendURL, stopBackend, err := backend.StartServer(ctx, os.Getenv(ui.EnvBackendLaddr), true)
+	if err != nil {
+		ui.HandleFatalPanic(cfg.App.Name, errors.Join(fmt.Errorf("could not start backend"), err))
+	}
+	defer stopBackend()
+
+	log.Println("Backend URL:", backendURL)
+
+	// Start the frontend
+	frontendURL, stopFrontend, err := frontend.StartServer(ctx, os.Getenv(ui.EnvFrontendLaddr), backendURL, true)
+	if err != nil {
+		ui.HandleFatalPanic(cfg.App.Name, errors.Join(fmt.Errorf("could not start frontend"), err))
+	}
+	defer stopFrontend()
+
+	log.Println("Frontend URL:", frontendURL)
+
+	for {
+		retry, err := ui.LaunchBrowser(
+			ctx,
+
+			frontendURL,
+			cfg.App.Name,
+			cfg.App.ID,
+
+			os.Getenv(ui.EnvBrowser),
+			os.Getenv(ui.EnvType),
+
+			ui.ChromiumLikeBrowsers,
+			ui.FirefoxLikeBrowsers,
+			ui.EpiphanyLikeBrowsers,
+			ui.LynxLikeBrowsers,
+
+			browserState,
+			ui.ConfigureBrowser,
+		)
+
+		if err != nil {
+			ui.HandleFatalPanic(cfg.App.Name, err)
+		}
+
+		if !retry {
+			return
+		}
+	}
+}
